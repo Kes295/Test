7e12f4cb27038c60e050536a1becfe3eaaf85447
Kes295 <kes_akarui295@mail.ru>
2015-05-15 12:47:16 +0300

    some text

---
+++ src/test/java/com/kes_akarui/webdriver/TextInput.java
@@ -0,0 +1,17 @@
+package com.kes_akarui.webdriver;
+
+import org.openqa.selenium.By;
+
+/**
+ * Created by Mandrake on 27.04.2015.
+ */
+public class TextInput extends Element {
+    public TextInput(By by){
+        this.by = by;
+    }
+
+    public void sendKeys(String value) {
+        element().clear();
+        element().sendKeys(value);
+    }
+}
